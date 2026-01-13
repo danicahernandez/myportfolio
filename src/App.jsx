7a3db4f88fbe9281175e1231c4a949786416bddc
danicahernandez <dgh2136@columbia.edu>
2026-01-13 09:58:16 -0500

    image attempt

--- src/App.jsx
+++ src/App.jsx
@@ -6,30 +6,27 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
+  return(
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h1 id = "name">
+        Danica Hernandez
+      </h1>
+      <h2 id = "purpose">
+        Jade Materials
+      </h2>
+      <a id = "notes" href ="https://docs.google.com/document/d/1GyxXn7rIKhLFDwFsDJBNSjZKBlu4vjut2HOlf69BAx4/edit?tab=t.0">
+        Notes
+      </a>
+      <a id = "notion" href = "https://azure-weeder-8fa.notion.site/JADE-2026-Web-Dev-0c6b99c0abf24676be1cb8e14b9e9ea2">
+        Notion
+      </a>
+      <img id = "background" src = "/Images/lowerfield.JPG" alt = "image"> 
+      </img>
+    </div>
     </>
   )
+
 }
 
 export default App
